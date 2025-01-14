b7c872aa3d01d4b44d59a84c4cb892befbc07ec1
Abbey Hankins <abbey@MacBook-Air.local>
2025-01-13 21:22:02 -0800

    user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "exists" do
-    User.new
+  it "has an email"  do
+    user = User.new
+    expect(user).to respond_to(:email)
+  end
+
+  it "has a role"  do
+    user = User.new
+    expect(user).to respond_to(:role)
   end
 
 end
