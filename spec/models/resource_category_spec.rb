6063f9d4181eb8e690c0c2b2846b4ea12a65dbcc
Patrick Punch <punchp@oregonstate.edu>
2025-01-15 14:57:18 -0800

    updated resource_category_spec.rb

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let(:res_cat) { ResourceCategory.new }
+
   it "exists" do
     ResourceCategory.new
   end
 
+  it "has a name" do
+    expect(res_cat).to respond_to(:name)
+  end
+
+  it "has and belongs to many organizations" do
+    expect(res_cat).to respond_to(:organizations)
+  end
+
+  it "has tickets" do
+    expect(res_cat).to respond_to(:tickets)
+  end
+  
 end
