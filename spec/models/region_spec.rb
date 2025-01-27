1b3613c0a40e285d7e8714647305d24e90cf1f82
Abbey Hankins <abbeyhankins57@gmail.com>
2025-01-27 15:31:24 -0800

    region validation tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe Region, type: :model do
     should have_many(:tickets)
   end
 
+  ## VALIDATION
+  describe "validation tests" do 
+
+    it "validates presence of name" do 
+      expect(region).to validate_presence_of(:name)
+    end
+
+    it "validates name length" do
+      expect(region).to validate_length_of(:name).is_at_most(255)
+      expect(region).to validate_length_of(:name).is_at_least(1)
+    end
+
+    it "validates uniqueness of name" do 
+      expect(region).to validate_uniqueness_of(:name).case_insensitive
+    end
+  end
+
 end
