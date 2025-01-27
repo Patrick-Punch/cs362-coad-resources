c5458ce80521b9b3cd80f919975e4ebfe3289b94
Patrick Punch <punchp@oregonstate.edu>
2025-01-27 15:43:29 -0800

    finished member func tests for region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,10 +1,8 @@
-#TODO
-
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  let(:region) { Region.new }
+  let(:region) { Region.new(name: "'Murica") }
 
   it "has a name" do
     expect(region).to respond_to(:name)
@@ -37,5 +35,17 @@ RSpec.describe Region, type: :model do
       expect(region).to validate_uniqueness_of(:name).case_insensitive
     end
   end
+  
+  ## MEMBER FUNCTION TESTS
+  describe "member function tests" do
 
+    it "can be unspecified" do
+      region = Region.unspecified
+      expect(region.name).to eq "Unspecified"
+    end
+
+    it "converts to string" do
+      expect(region.to_s).to eq "'Murica"
+    end
+  end
 end
