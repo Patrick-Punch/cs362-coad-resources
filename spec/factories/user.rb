12350135e2926b267f4dc859b95480828e286413
Abbey Hankins <abbeyhankins57@gmail.com>
2025-02-10 14:55:40 -0800

    comments on users and trying to fix it kinda but I only commented on it bc it didnt work when I ran rsepc

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,21 @@ FactoryBot.define do
     factory :user do
         email { 'example@hotmail.com' }
         password { 'password123' }
-        role { :organization }
+        #before( :create) { |user| user.skip_confirmation! }
+
+        #trait :organization_approved do
+            role { :organization }
+            # organization_id { create (:orgainization, :approved).id}
+        #end
+
+        #trait :orgainization_unapproved do 
+            #role { :organization}
+            #organization_id { create (:organization).id}
+        #end
+
+        #trait :admin do
+            # role { :admin}
+        #end
     end
 
 
