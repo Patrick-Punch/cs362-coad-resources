a2cd41753ecf0483c23ea9d2d2c30db21e950e61
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-02-15 15:19:12 -0800

    After only a few millenia, tickets_controller.rb has become tested

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -17,37 +17,29 @@ RSpec.describe TicketsController, type: :controller do
     end
 
     it 'show' do
-      #TODO expect to not work because not signed in
-      pending
       get(:show, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
 
     it 'capture' do
-      #TODO expect to not work because not signed in
-      pending
-      raise "fail"
       post(:capture, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
 
     it 'release' do
-      #TODO expect to not work because not signed in
-      pending
-      raise "fail"
       post(:release, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
 
     it 'close' do
-      #TODO expect to not work because not signed in
-      pending
-      raise "fail"
       patch(:close, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
 
     it 'destroy' do
-      #TODO expect to not work because not admin
-      pending
-      raise "fail"
+      #TODO test that it didn't actually destroy a ticket
       delete(:destroy, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
   end
 
@@ -57,31 +49,86 @@ RSpec.describe TicketsController, type: :controller do
 
     before(:each) { sign_in user }
 
+    it 'create' do
+      post(:create, params: { ticket: attributes_for(:ticket) })
+      expect(response).to redirect_to ticket_submitted_path
+    end
+
+    it 'new' do
+      expect(get(:new)).to be_successful
+    end
+
     it 'show' do
       get(:show, params: { id: ticket.id })
       expect(response).to redirect_to dashboard_path
-
-      # TODO expect not to redirect if user_organization is not approved
+      #TODO test when organization.approved?
     end
 
     it 'capture' do
-      # TODO
       post(:capture, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
+      #TODO test when organization.approved?
+    end
+
+    it 'release' do
+      post(:release, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
+      #TODO test when organization.approved?
+    end
+
+    it 'close' do
+      patch(:close, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
+      #TODO test when organization.approved?
     end
 
     it 'destroy' do
-      #TODO expect to not work because not admin
-      pending
-      raise "fail"
+      #TODO test that it didn't actually destroy a ticket
       delete(:destroy, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
     end
   end
 
   describe 'as an admin' do
-    let(:user) { FactoryBot.create(:user, :admin) }
+    let(:user) { create(:user, :admin) }
+    let(:ticket) { create(:ticket) }
     before(:each) { sign_in user }
 
-    it { expect(get(:new)).to be_successful }
-  end
+    it 'create' do
+      post(:create, params: { ticket: attributes_for(:ticket) })
+      expect(response).to redirect_to ticket_submitted_path
+    end
 
+    it 'new' do
+      expect(get(:new)).to be_successful
+    end
+
+    it 'show' do
+      get(:show, params: { id: ticket.id })
+      expect(response).to be_successful
+    end
+
+    it 'capture' do
+      post(:capture, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
+      #TODO test other possibilites
+    end
+
+    it 'release' do
+      post(:release, params: { id: ticket.id })
+      expect(response).to redirect_to dashboard_path
+    end
+
+    it 'close' do
+      patch(:close, params: { id: ticket.id })
+      expect(response).to redirect_to "#{dashboard_path}#tickets:open"
+      #TODO test other possibilities
+    end
+
+    it 'destroy' do
+      #TODO test that it actually deleted ticket
+      delete(:destroy, params: { id: ticket.id })
+      expect(response).to redirect_to "#{dashboard_path}#tickets"
+    end
+  end
 end
