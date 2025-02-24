5c30dac7d2ab5f3c020c370d70628c1436ffeb09
Patrick Punch <punchp@oregonstate.edu>
2025-02-24 14:58:10 -0800

    added code from class and in class challenge

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  
+  before do
+    @region = create(:region)
+    @resource_category = create(:resource_category)
+    @admin = create(:user, :admin)
+  end
 
+  it 'can be created from the home screen' do
+    log_in_as(@admin)
+
+    visit root_path
+
+    click_on 'Regions'
+    click_on 'Add Region'
+
+    fill_in 'Name', with: 'Test Name'
+
+    click_on 'Add Region'
+
+    expect(current_path).to eq '/regions'
+  end
 end
