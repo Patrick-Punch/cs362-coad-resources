4a62751ab868619ced27f7f9b9b21919a7e29f45
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-03-10 16:07:14 -0700

    finished two more feature tests

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  before do
+    @admin = create(:user, :admin)
+    @region = create(:region)
+  end
 
+  it 'works' do
+    log_in_as(@admin)
+    visit root_path
+    click_on 'Regions'
+    click_on @region.name
+    click_on 'Delete'
+    expect(current_path).to eq regions_path
+  end
 end
--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+  it 'works' do
+    user = create(:user)
+    visit root_path
+
+    click_on 'Log in'
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: user.password
+    find('#commit').click
+    expect(current_path).to eq dashboard_path
+
+  end
+
 end
