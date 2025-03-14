27ea4e9a62c19e5eadd5e229c854c760912c6365
Abbey Hankins <abbeyhankins57@gmail.com>
2025-03-14 14:11:55 -0700

    reject org end to end

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+    before do
+        @organization = create(:organization)
+        @admin = create(:user, :admin)
+    end
+
+    it 'organization can be rejected' do
+        log_in_as(@admin)
+
+        visit root_path
+
+        click_on "Organizations"
+        click_on "All"
+        click_on @organization.name
+        click_on "Reject"
+
+        expect(current_path).to eq '/organizations'
+    end 
 
 end
+
+
+
--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+    # before do 
+    #     @organization = create(:organization)
+    #     @admin = create(:user, :admin)
+    # end 
+
+    # it "can update an organization" do
+    #     log_in_as(@admin)
+
 
 end
