aecacf13094305d8aae4ef6ac68fa560b7f8861e
Abbey Hankins <abbeyhankins57@gmail.com>
2025-03-14 15:45:06 -0700

    update_organization end to end working

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-    # before do 
-    #     @organization = create(:organization)
-    #     @admin = create(:user, :admin)
-    # end 
+    before do 
+        @user = create(:user, :organization_approved)
+        @fake_name = 'FAKE'
+    end 
 
-    # it "can update an organization" do
-    #     log_in_as(@admin)
+    it "can update an organization" do
+        
+        log_in_as(@user)
 
+        visit root_path
 
+        click_on 'Dashboard'
+
+        click_on 'Edit Organization'
+
+        fill_in 'Name', with: @fake_name
+        fill_in 'Phone Number', with: '555-555-5555'
+        fill_in 'Email', with: 'fake@gmail.com'
+        fill_in 'Description', with: 'Boring stuff'
+
+        expect(current_path).to be organizations.id
 end
+
